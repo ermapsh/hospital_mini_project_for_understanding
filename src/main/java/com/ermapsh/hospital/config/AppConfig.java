b61d017c09f244650747009d0259ba1961b07ed7
mahesh <>
2026-06-17 23:08:57 +0530

    update

---
+++ src/main/java/com/ermapsh/hospital/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.ermapsh.hospital.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AppConfig {
+
+    @Bean
+    ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+}

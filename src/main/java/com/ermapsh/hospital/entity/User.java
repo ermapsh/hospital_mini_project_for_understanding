6b343b32900cb6de621d3b9ee12e2bc1982ce226
mahesh <>
2026-06-22 15:07:33 +0530

    update over spring security

---
+++ src/main/java/com/ermapsh/hospital/entity/User.java
@@ -0,0 +1,21 @@
+package com.ermapsh.hospital.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
